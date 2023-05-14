06ef9476facee890f4bd4e1a45d522a350abeb3e
nrsabit <mailbox.sabit@gmail.com>
2023-05-14 19:47:49 +0600

    server ready for desktop

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ app.use(cors())
 app.use(express.json())
 
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h7lvo9z.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h7lvo9z.mongodb.net/?retryWrites=true&w=majority`;
 
-const uri = `mongodb+srv://carDoctor:52P6MevaPpQ4LQpJ@cluster0.h7lvo9z.mongodb.net/?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://carDoctor:52P6MevaPpQ4LQpJ@cluster0.h7lvo9z.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
